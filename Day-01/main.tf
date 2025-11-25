2c925cb87faa0d1665b117e0461e5c6a5059a711
Amitkushwaha7 <amitkumarsingh843412@gmail.com>
2025-11-25 17:57:58 +0530

    Update the Day-01 and Day-02 learnings

--- Day-01/main.tf
+++ Day-01/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0e472ba40eb589f49"   # Amazon Linux 2 AMI (ap-south-1)
+  ami           = "ami-0fa3fe0fa7920f68e"   # Amazon Linux 2 AMI (ap-south-1)
   instance_type = "t2.micro"                # Free-tier eligible
 
   tags = {
